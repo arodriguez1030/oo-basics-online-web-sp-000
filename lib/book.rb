239daaad79cabb5e32a90b8825ec93dd0faf63ef
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-22 15:21:39 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,9 +6,6 @@ class Book
   def title
     @title 
   end
-  attr_accessor :author
-  attr_accessor :page_count
-  attr_accessor :genre
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
